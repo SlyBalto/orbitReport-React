dcadf862a3f1054e43bf2951339b5acea1c718b8
Sly <SlyBalto@Gmail.com>
2023-08-09 18:28:37 -0500

    npm tests successful!

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,27 @@
-const Buttons = () => {
+import satData from './satData'
+import './styling.css'
+
+const Buttons = ({filterByType, setSat, displaySats}) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className='flex-container'>
+      {/* automatically creates buttons */}
+    {displaySats.map((sat, id) => {
+      return (
+        <button onClick={() => filterByType(sat)} key={id}>
+        
+          {sat} Orbit
+        </button>
+        
+      );
+    })}
+
+      {/* this version invokes setSat immeadiately. some sources say this is not preferred, 
+      since it sets the state right as the component renders instead of just on the click.
+            <button onClick={setSat(satData)}>All Orbits</button>*/}
+
+      <button onClick={() => setSat(satData)}>All Orbits</button>
       </div>
   );
-};
+};  
 
 export default Buttons;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,34 @@
+import './styling.css'
+
 const Table = ({ sat }) => {
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Launch Date</th>
+          <th>Operational</th>
         </tr>
-        </thead>
+      </thead>
+      
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {/* interates through 'sat' object array, takes each object's data, and 
+          places them into corresponding locations using index from .map() */}
+          {sat.map((sat, index) => (
+            <tr key={index}>
+              <td>{sat.name}</td>
+              <td>{sat.type}</td>
+              <td>{sat.launchDate}</td>
+              <td>{sat.operational ? 'Active' : 'Inactive'}</td>
+            </tr>
+          ))}
+
+        
+        
         </tbody>
       </table>
   );
 };
-
+// TODO: <th> for: Name, Type of Satellite, Launch Date, Status
 export default Table;
